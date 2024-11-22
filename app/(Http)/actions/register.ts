2feef544f21420315ef1fb2558eea904a71bd408
Godwin <godwinsawyer@gmail.com>
2024-11-22 23:28:50 +0100

    created the user

--- app/(Http)/actions/register.ts
+++ app/(Http)/actions/register.ts
@@ -1,11 +1,31 @@
 "use server";
-import * as z from "zod";
+import UserModel from "@/app/models/UserModel";
+import prisma from "@/lib/database/client";
 import { RegisterSchema } from "@/lib/validation";
+import bcrypt from "bcryptjs";
+import * as z from "zod";
 
-export const register = async (values: z.infer<typeof RegisterSchema>) => {
-    const validation = RegisterSchema.safeParse(values);
-    if (!validation.success)
-        return { error: "Invalid fields" };
-    return {success: "Email sent"}
+export async function register(values: z.infer<typeof RegisterSchema>) {
+
+  const validation = RegisterSchema.safeParse(values);
+  if (!validation.success) return { error: "Invalid fields" };
+
+  const { email, name, password } = validation.data;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  try {
+    const user = await UserModel.getUserByEmail(email)
+    if (user) return { error: "Email already in use" };
+
+    await prisma.user.create({
+      data: { name, email, password: hashedPassword },
+    });
+
+    return { success: "User Created" };
+  } catch {
+    return { error: "An unexpected error occurred." };
+  }
 }
+
 // starTransition would be useful if i any next cache or revalidation or redirects here revalidatePath or revalidateTag you cant cache the end of then using .then but startTransition can tell you when the post has ended
--- app/models/UserModel.ts
+++ app/models/UserModel.ts
@@ -1,5 +1,22 @@
+import prisma from "@/lib/database/client";
+
 const UserModel = {
-  async UserModel() {},
+  async getUserByEmail(email: string) {
+    try {
+      const user = await prisma.user.findUnique({ where: { email } });
+      return user;
+    } catch {
+      return null;
+    }
+  },
+  async getUserById(id: string) {
+    try {
+      const user = await prisma.user.findUnique({ where: { id } });
+      return user;
+    } catch {
+      return null;
+    }
+  },
 };
 
 export default UserModel;
