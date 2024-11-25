bbeeb90104c587b04c34c98bd1122effed1b5841
Godwin <godwinsawyer@gmail.com>
2024-11-25 03:31:24 +0100

    Created the error page for the authentication

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -1,19 +1,17 @@
 import Header from "@/components/auth/header";
 import { BackButton } from "@/components/auth/back-button";
-import {
-  CardHeader,
-  CardFooter,
-  Card,
-} from "@/components/ui/card";
+import { CardHeader, CardFooter, Card } from "@/components/ui/card";
 const ErrorCard = () => {
-    return <Card className="w-[400px] shadow-md">
-        <CardHeader>
-            <Header label="Oops! something went wrong!" />
-            <CardFooter>
-                <BackButton href="/auth/login" label="Back to login" />
-            </CardFooter>
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label="Oops! something went wrong!" />
+        <CardFooter>
+          <BackButton href="/auth/login" label="Back to login" />
+        </CardFooter>
       </CardHeader>
-  </Card>;
+    </Card>
+  );
 };
 
 export default ErrorCard;
